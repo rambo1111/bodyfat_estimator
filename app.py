ca1ee4d9ec1765ffa7b5072253901f9e8d91ed09
rambo1111 <vibhawspark@gmail.com>
2024-03-01 18:31:55 +0530

    first commit

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def predict():
     # Render the result
     return render_template('index.html', prediction=prediction[0])
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
